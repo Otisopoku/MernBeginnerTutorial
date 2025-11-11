8f50054f8b375ae64f4a1177ac003b73b147a6ae
Otis Opoku <otisopoku30@gmail.com>
2025-11-11 01:24:35 +0000

    add sorting by newly created first

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -14,7 +14,7 @@ export const getNoteById = async (req, res) => {
 
 export const getAllNotes = async (req, res) => {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({ createdAt: -1 }); // newest first
     res.status(200).json(notes);
   } catch (error) {
     console.error("Error in getAllNotes controller", error);
