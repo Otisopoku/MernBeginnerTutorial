52a73a7c94729dc701cbea197fe2a5327bc4965c
Otis Opoku <otisopoku30@gmail.com>
2025-11-12 22:08:04 +0000

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,22 +4,37 @@ import { connectDB } from "./config/db.js";
 import dotevn from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotevn.config();
 
 const app = express();
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    // without specifying the origin, it will allow interaction with any client
-    origin: "http://localhost:5173",
-  })
-);
+// use cors when we are in production because we are using different domains for the backend end server and frontend
+if (process.env.NODE_ENV === "development") {
+  app.use(
+    cors({
+      // without specifying the origin, it will allow interaction with any client
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 app.use("/api/notes", router);
 
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  // serve the front end statically since we are deploying both the front end and backend on one domain on ra
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB()
   .then(
